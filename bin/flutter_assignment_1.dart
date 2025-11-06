8906206b4e5f6fe35f106166d50f613df1d778fe
rezen <rezen.khadgi@gmail.com>
2025-11-06 17:44:11 +0545

    inital commit

---
+++ bin/flutter_assignment_1.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_assignment_1/flutter_assignment_1.dart' as flutter_assignment_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_assignment_1.calculate()}!');
+}
---
+++ test/flutter_assignment_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_assignment_1/flutter_assignment_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
